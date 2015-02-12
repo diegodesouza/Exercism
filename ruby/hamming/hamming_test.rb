f4592161c47062ac4c52c3be16e34fee4c15de91
Diego De Souza <diegotrus7@gmail.com>
2015-02-11 20:54:30 -0500

    all tests passing

--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -33,17 +33,14 @@ class HammingTest < MiniTest::Unit::TestCase
   end
 
   def test_nonunique_characters_within_second_strand
-    skip
     assert_equal 1, Hamming.compute('AGG', 'AGA')
   end
 
   def test_large_hamming_distance
-    skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
   def test_hamming_distance_in_very_long_strand
-    skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 end
