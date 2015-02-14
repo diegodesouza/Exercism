8ef2286c47e10580cb2fcd6b4220f91c354e00b9
Diego De Souza <diegotrus7@gmail.com>
2015-02-14 11:54:55 -0500

    all tests passing

--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -10,19 +10,16 @@ class GigasecondTest < Minitest::Unit::TestCase
   end
 
   def test_2
-    skip
     gs = Gigasecond.from(Time.utc(1977, 6, 13))
     assert_equal Time.utc(2009, 2, 19, 1, 46, 40), gs
   end
 
   def test_3
-    skip
     gs = Gigasecond.from(Time.utc(1959, 7, 19))
     assert_equal Time.utc(1991, 3, 27, 1, 46, 40), gs
   end
 
   def test_4_with_seconds
-    skip
     gs = Gigasecond.from(Time.utc(1959, 7, 19, 23, 59, 59))
     assert_equal Time.utc(1991, 3, 28, 1, 46, 39), gs
   end
