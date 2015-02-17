dc9f455b93db2830c693776456355d3c62983dce
Diego De Souza <diegotrus7@gmail.com>
2015-02-16 22:35:27 -0500

    first test passing

---
+++ ruby/rna-transcription/complement.rb
@@ -0,0 +1,12 @@
+class Complement
+  DNA_RNA = {
+    'G' => 'C',
+    'C' => 'G',
+    'T'=> 'A',
+    'A'=> 'U'
+  }
+
+  def self.of_dna(dna)
+    dna.chars.map { |string| DNA_RNA.fetch(string) }.join
+  end
+end
